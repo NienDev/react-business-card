f9606559989af1de7a9bd141ad38554df33cca96
NienDev <niewebdev@gmail.com>
2022-09-04 20:46:28 +0700

    Adding dist

--- src/components/Image.jsx
+++
@@ -1,7 +0,0 @@
-export default function Image(props) {
-  return(
-    <div className="rounded-t-xl truncate">
-      <img src={props.url} alt="image" aria-hidden="true" className="max-w-full"/>
-    </div>
-  )
-};
--- src/components/Info.jsx
+++
@@ -1,9 +0,0 @@
-export default function Info(props) {
-  return (
-    <div className="text-center">
-      <h1 class="text-3xl font-bold">{props.name}</h1>
-      <h3 class="text-lg text-ly mb-1">{props.job}</h3>
-      <p className="text-xs">{props.email}</p>
-    </div>
-  )
-}
